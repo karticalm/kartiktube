5d3bc661f4b70f9fb6ed3dc88807a65d08772fc1
karticalm <121007023+karticalm@users.noreply.github.com>
2023-03-26 10:15:19 +0530

    Initial Skeleton

---
+++ src/Components/ButtonTags.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ButtonTags = () => {
+  return <div>ButtonTags</div>;
+};
+
+export default ButtonTags;
---
+++ src/Components/VideoContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ButtonTags from "./ButtonTags";
+import VideoList from "./VideoList";
+
+const VideoContainer = () => {
+  return (
+    <div>
+      <ButtonTags />
+      <VideoList />
+    </div>
+  );
+};
+
+export default VideoContainer;
